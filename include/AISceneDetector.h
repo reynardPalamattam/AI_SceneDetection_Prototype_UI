ce06bd43af10269b865c7d5731e5278692b59363
Reynard Wilson Palamattam <reynardwilson11@gmail.com>
2025-10-27 14:46:10 +0530

    Add header for AiSceneDetector class

--- include/AISceneDetector.h
+++ include/AISceneDetector.h
@@ -1,1 +1,18 @@
-doijd
+#ifndef AI_SCENE_DETECTOR_H
+#define AI_SCENE_DETECTOR_H
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <string>
+#include <vector>
+
+class AiSceneDetector{
+  public : 
+    AISceneDetector(const std::string& modelPath);
+    std::string detectScene(const cv::Mat& frame);
+  private:
+    cv::dnn::Net net;
+    std::vector<std::string> sceneLabels;
+};
+
+#endif
